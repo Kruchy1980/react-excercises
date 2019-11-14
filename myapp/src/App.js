36ca108d932da61f0c206c6232c1c7fa7834531c
Kruchy1980 <jakub.kruszynski@vp.pl>
2019-11-14 23:04:41 +0100

    First React Application - Beginning - with components creating and importing

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,67 +1,27 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+// Here we do have imports of necessary packages to our application
+import React, { Component } from 'react';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+import './App.css';
 
-// export default App;
-import React from 'react'
-import ReactDOM from 'react-dom'
+// This is an element extending Components and is injected into App.js file in src folder.  - we can display it by running npm start in terminal in our app directly
+class App extends Component { 
+  // Render is a methode of react as well
+  render() {
+    return (
+      // (That is first element of our webApp)
+      <div className='App'>
+        <h1>Hello All I am React Application</h1>
+        <p>That is really working :)</p>
+      </div>
+    );
+    // We can use createElement methode but it is not used to often, because the code is longer and means the same what aboved code
+    //React.createElement takes 3 arguments:
+    //first - what we want to create
+    //second - is a configuration of first element and is oprional can be null as well but also an JS object can be injected
+    // third - is an any amount of children and i there is more than one they must be separated by coma. Childrens means what is nested in this element in our case  in div but not used because of unnecessary complcation of code
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1'), null, 'Hi I\'m a React Application!!!');
+  }
+};
 
-// To not repeat the block code in Html after importing React by scripts in HTML code - one of the methodes to implement React to uor code we can start to create the components in js files like that
-// create function - also one of the ways - names have to be named with Capsized letters
-// By adding in function an argument props we can later change the values of component in our HTML code
-function Person(props) {
-// creaate return of function with brackets to multiply arguments
-return (
-  // We have to use a box in here to implement multi objects in one box
-  // We have to remember that rezct classes can not be named as same as the classes in HTML so we need to change the name for className for instance
-  // We can use the specified values using curly brackets in HTML tags to display them in HTML Code
-  <div className='Person'>
-    <h1>Twoje imię: {props.name}</h1>
-    <p>Twój wiek: {props.age}</p>
-  </div>
-)
 
-}
-
-//  We can add some code above the render to include the two components in one to make the code shorter  by adding the variable with cumulated code
-
-const App = () => (
-  <div>
-    <div>
-      <Person name='Jurek' age='30' />
-      "<Person name='Heniu' age='25' />"
-    </div>
-  </div>
-);
-
-// We had also implemented rect dom the library responsible for actions with DOM in HTML.
-// Now we can render a function = implement function to particular element in HTML by render below by implementing it to specified element chosen by querySelector
-// If we use the props we can add attributge to our render function to implement them later dinamically to HTML code
-// ReactDOM.render(<Person name='Jurek' age='30' />, document.querySelector('#p1'))
-// Now by getting the other element of our HTML we can set the second name by using similar parameters to our second render function
-// ReactDOM.render(<Person name='Heniu' age='25' />, document.querySelector('#p2'))
-// Now we can tqke from dom the onlu one div which we created
-ReactDOM.render(<App />, document.querySelector('#root'));
-
-export default App
+export default App;
