a9c2dd398f695d566838a615a7d0828079d90bf3
Kruchy1980 <jakub.kruszynski@vp.pl>
2019-11-15 20:50:25 +0100

    Adding dynamic part inside injected texts

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,29 +5,29 @@ import './App.css';
 // It is how  we can add the separate components to the app app.js until we do not create th Root.js file and won't add there paths to another files with components.
 import Person from './Person/Person'; // We can use capital letter of our component and rlative path to our newly created file Person.js The name of import has to be named by capital leter
 
-
-
 // This is an element extending Components and is injected into App.js file in src folder.  - we can display it by running npm start in terminal in our app directly
-class App extends Component { 
-  // Render is a methode of react as well
-  render() {
-    return (
-      // (That is first element of our webApp)
-      // Now we can use the component we nhave create in App.js file using JSX tags which are look similar to HTML tags
-      <div className='App'>
-        <h1>Hello All I am React Application</h1>
-        <p>That is really working :)</p>
-        <Person />
-      </div>
-    );
-    // We can use createElement methode but it is not used to often, because the code is longer and means the same what aboved code
-    //React.createElement takes 3 arguments:
-    //first - what we want to create
-    //second - is a configuration of first element and is oprional can be null as well but also an JS object can be injected
-    // third - is an any amount of children and i there is more than one they must be separated by coma. Childrens means what is nested in this element in our case  in div but not used because of unnecessary complcation of code
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1'), null, 'Hi I\'m a React Application!!!');
-  }
-};
-
+class App extends Component {
+	// Render is a methode of react as well
+	render() {
+		return (
+			// (That is first element of our webApp)
+			// Now we can use the component we nhave create in App.js file using JSX tags which are look similar to HTML tags
+			<div className="App">
+				<h1>Hello All I am React Application</h1>
+				<p>That is really working :)</p>
+				<Person />
+				<Person />
+				<Person />
+				<Person />
+			</div>
+		);
+		// We can use createElement methode but it is not used to often, because the code is longer and means the same what aboved code
+		//React.createElement takes 3 arguments:
+		//first - what we want to create
+		//second - is a configuration of first element and is oprional can be null as well but also an JS object can be injected
+		// third - is an any amount of children and i there is more than one they must be separated by coma. Childrens means what is nested in this element in our case  in div but not used because of unnecessary complcation of code
+		// return React.createElement('div', {className: 'App'}, React.createElement('h1'), null, 'Hi I\'m a React Application!!!');
+	}
+}
 
 export default App;
--- myapp/src/Person/Person.js
+++ myapp/src/Person/Person.js
@@ -1,9 +1,7 @@
 // B4 we can start to use components we have to import the React from ract to interprete the part of code.
 import React from 'react';
 
-
 // We need to connect our component with App by importing
-
 
 // Now when we created the new folder in source folder we can create next view in our project - Basics:
 //Use the capital letter to create folders as well as .js files which are not Database files which can be named camerCase style.
@@ -11,14 +9,15 @@ import React from 'react';
 // Because of ES6 when we have new opportunities we can use const for non changing module wchich is mostly used and we can use the lower letters as the name as well as large. Theoretically lower letters are mostly used in names.
 // () in function is called arguments list and is used instead of sual function name as the arrow function.
 // this is component
+// const person = () => {
+// 	//Component function has to return some JSX eg.
+// 	return <p>I'am a Person</p>;
+// };
+// Creating of dynamic content
 const person = () => {
-    //Component function has to return some JSX eg.
-    return <p>I'am a Person</p>;
+	//Component function has to return some JSX eg. - for eg we want to randomize age in every injected component in App.js if we want to injest dynamic part of code we have to make it in the curly brackets.
+	return <p>I'am a Person and I am {Math.floor(Math.random() * 30)} years old</p>;
 };
-
-
-
 
 // We need to export the component as well so we could use the component  we crated
 export default person;
-
