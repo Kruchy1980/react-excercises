6d41ee4c5fa6246ad15e15f11d601e0069befc60
Kruchy1980 <jakub.kruszynski@vp.pl>
2019-11-19 21:12:25 +0100

    Asignement of states to components

--- mycomponents/mycomponents/src/App.js
+++ mycomponents/mycomponents/src/App.js
@@ -2,20 +2,39 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 // import UserInput from './components/UserInput';
-import UserOutput from './components/UserOutput';
+import UserOutput from './UserOutput/UserOutput';
+import UserInput from './UserInput/UserInput';
 
-function App() {
-  return (
-    <div className="App">
-      <UserOutput />
-      <UserOutput />
-      <UserOutput />
-      <UserOutput />
-      <UserOutput />
-      <UserOutput />
-      
-    </div>
-  );
+class App extends React.Component {
+  state ={
+    username: 'Dżejkob'
+  }
+
+usernameChangeHandler = (event) => {
+  this.setState({
+    username: event.target.value
+  })
 }
+
+  render() {
+    return (
+      <div className="App">
+        <UserInput changed={this.usernameChangeHandler}
+        currentName={this.state.username}/>
+        <UserOutput userName={this.state.username}/>
+        <UserOutput userName={this.state.username}/>
+        <UserOutput userName={this.state.username}/>
+        <UserOutput userName={this.state.username}/>
+        <UserOutput userName={this.state.username}/>
+
+        <UserOutput userName="Helmut" />
+
+      </div>
+    );
+  }
+}
+
+
+
 
 export default App;
---
+++ mycomponents/mycomponents/src/UserOutput/UserOutput.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './UserOutput.module.css';
+
+const UserOutput = props => {
+    return (
+        <div className={styles.Paragraph}>
+            <p>My name is {props.userName}</p>
+            <p>And that is all you need to know about me</p>
+        </div>
+    )
+}
+
+
+
+export default UserOutput;
