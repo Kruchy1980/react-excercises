2f6a897b4688a634f8272d7a2ad7f6b248b33baf
Kruchy1980 <jakub.kruszynski@vp.pl>
2019-11-19 20:27:47 +0100

    Initial of second project with basics included

---
+++ mycomponents/mycomponents/src/components/UserInput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserInput = props => {
+    return (
+        <div>
+            <input type="text" placeholder="Enter your Name"/>
+        </div>
+    )
+}
+
+
+export default UserInput;
