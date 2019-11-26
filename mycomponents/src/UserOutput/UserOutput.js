db22262aadd3fe2243645df40d45ecde663b0641
Kruchy1980 <jakub.kruszynski@vp.pl>
2019-11-26 20:40:11 +0100

    Styles of text changed and dynamically rendering of name added

--- mycomponents/src/UserOutput/UserOutput.js
+++ mycomponents/src/UserOutput/UserOutput.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import styles from './UserOutput.module.css';
+import React from 'react'
+import styles from './UserOutput.module.css'
 
 const UserOutput = props => {
-    return (
-        <div className={styles.Paragraph}>
-            <p>My name is {props.userName}</p>
-            <p>And that is all you need to know about me</p>
-        </div>
-    )
+  return (
+    <div className={styles.UserOutput}>
+      <p>My name is {props.userName}</p>
+      <p>And that is all you need to know about me</p>
+    </div>
+  )
 }
 
-
-
-export default UserOutput;
+export default UserOutput
