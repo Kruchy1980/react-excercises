c719b2b9e243934faaddd849742497405915eaeb
Kruchy1980 <jakub.kruszynski@vp.pl>
2019-11-26 20:03:09 +0100

    ReactComponent Excercisess

--- mycomponents/src/UserInput/UserInput.js
+++ mycomponents/src/UserInput/UserInput.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from 'react'
 
 const UserInput = props => {
-    const inputStyle ={
-        border: '2px solid green',
-        shadowBox: '1px 5px 10px #262466',
-    }
-    return (
-        <div>
-            <input
-            style={inputStyle} 
-            type="text" 
-            onChange={props.changed} 
-            value={props.currentName}/>
-        </div>
-    )
+  const inputStyle = {
+    border: '2px solid green',
+    shadowBox: '1px 5px 10px #262466'
+  }
+  return (
+    <div>
+      <input
+        style={inputStyle}
+        type='text'
+        onChange={props.changed}
+        value={props.currentName}
+      />
+    </div>
+  )
 }
 
-
-export default UserInput;
+export default UserInput
